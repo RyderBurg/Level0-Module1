e452638702fb807e20f3ced1568443ba8baa45ef
league <league@imac-24.attlocal.net>
2024-06-28 11:57:28 -0700

    jyufmuthdmuy

---
+++ src/_03_if_else/_1_unbirthday/unbirthday.java
@@ -0,0 +1,10 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class unbirthday{
+	public static void main(String[] args) {
+		JOptionPane.showInputDialog("what is your birthday");
+		if(input.equals("06/24");
+	}
+	}
